92e5d9303a761bf61c8fdbfc0f9db4977cdce40d
dcw <-->
2025-08-16 13:48:22 -0400

    ...

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
 # Install Clojure CLI tools
-curl -O https://download.clojure.org/install/linux-install-1.11.1.1429.sh
-chmod +x linux-install-1.11.1.1429.sh
-sudo ./linux-install-1.11.1.1429.sh
-rm linux-install-1.11.1.1429.sh
+curl -L -O https://github.com/clojure/brew-install/releases/latest/download/linux-install.sh
+chmod +x linux-install.sh
+sudo ./linux-install.sh
+rm linux-install.sh
 
 # Install Leiningen
 curl -O https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
 chmod +x lein
 sudo mv lein /usr/local/bin/
-lein version
 
-# Install Babashka (fast Clojure scripting)
-sudo bash < <(curl -s https://raw.githubusercontent.com/babashka/babashka/master/install)
-
-# Install common web development tools
+# Install shadow-cljs
 npm install -g shadow-cljs
-npm install -g live-server
 
-echo "Clojure development environment setup complete!"
+echo "Clojure development environment ready!"
